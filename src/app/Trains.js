25b6818271d93bb5526fe3cebf9d469d7babd34b
lmr@hainatravel.com <59361885@qq.com>
2017-05-09 09:35:39 +0800

    '分离routes逻辑到单独的routes文件夹中'

--- src/app/Trains.js
+++ src/app/Trains.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom';
-import TrainIndex from '../layouts/TrainIndex';
-import TrainCity from '../layouts/TrainCity';
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { routes } from '../routes/Trains';
 
+//静态资源
 import '../resources/css/TrainIndex.css';
 import '../resources/css/Motion.css';
 import '../resources/png/city.png';
@@ -18,53 +17,11 @@ class Trains extends React.Component {
   //console.log(props);
   }
 
-  routes = (props) => {
-
-    //传递redux和router的值
-    const location = props.location;
-
-    //页面路由表
-    const componentArray = [
-      { to: '/index', component: TrainIndex, name: 'index' },
-      { to: '/city', component: TrainCity, name: 'city' },
-    ];
-
-    //使用当前path遍历路由表，转化取出对应的组件
-    const component = componentArray.map(item => {
-      if (location.pathname === item.to) {
-      return item.component;
-      }
-    }).filter(item => item)[0];
-
-    //首页路由
-    const homeRoute = () => (
-      <Redirect to="/index"/>
-    );
-
-    return (
-      <div>
-        <Route exact path="/" render={homeRoute} />
-        <CSSTransitionGroup transitionName="page" transitionEnterTimeout={200} transitionLeaveTimeout={200}>
-          <Route location={location} key={location.pathname} path={location.pathname} component={component} />
-        </CSSTransitionGroup>
-      </div>
-    );
-  }
-
   render() {
     return (
       <Router>
-        <Route render={this.routes} />
+        <Route render={routes} />
       </Router>
-      /*
-      <Router>
-        <CSSTransitionGroup transitionName="page" transitionEnterTimeout={200} transitionLeaveTimeout={200}>
-          <Route path="/index" key="index" component={TrainIndex}/>
-          <Route path="/city" key="city" component={TrainCity}/>
-          <Redirect to="/index"/>
-        </CSSTransitionGroup>
-      </Router>
-      */
     );
   }
 
--- src/routes/Trains.js
+++ src/routes/Trains.js
@@ -1,8 +1,39 @@
-import City from '../layouts/City';
+import React from 'react';
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { Route } from 'react-router-dom';
+import TrainIndex from '../layouts/TrainIndex';
+import TrainCity from '../layouts/TrainCity';
 
-export const routes = [
-    {
-        path: '/fzcity/',
-        component: City
+export const routes = (props) => {
+
+  //传递redux和router的值
+  const location = props.location;
+
+  //页面路由表
+  const componentArray = [
+    { to: '/index', component: TrainIndex, name: 'index' },
+    { to: '/city', component: TrainCity, name: 'city' },
+  ];
+
+  //使用当前path遍历路由表，转化取出对应的组件
+  const component = componentArray.map(item => {
+    if (location.pathname === item.to) {
+    return item.component;
     }
-];
+  }).filter(item => item)[0];
+
+  //首页跳转
+  const homeRoute = () => (
+    <Redirect to="/index"/>
+  );
+
+  return (
+    <div>
+      <Route exact path="/" render={homeRoute} />
+      <CSSTransitionGroup transitionName="page" transitionEnterTimeout={200} transitionLeaveTimeout={200}>
+        <Route location={location} key={location.pathname} path={location.pathname} component={component} />
+      </CSSTransitionGroup>
+    </div>
+  );
+
+}
