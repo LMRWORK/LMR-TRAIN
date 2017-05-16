85add31ef7565926ed4fc4b684e9bc63a2826b6c
lmr@hainatravel.com <59361885@qq.com>
2017-05-16 11:32:55 +0800

    '测试rn-listview'

--- src/layouts/TrainSearch.js
+++ src/layouts/TrainSearch.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import { NavBar, WhiteSpace, WingBlank, Toast, List} from 'antd-mobile';
+import { NavBar, WhiteSpace, WingBlank, Toast, ListView } from 'antd-mobile';
 import { connect } from 'react-redux';
 import { fetchTrains } from '../actions/Trains';
+
+//测试数据
+const data = [
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+    title: 'Meet hotel',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+    title: 'McDonald\'s invites you',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+  {
+    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+    title: 'Eat the week',
+    des: '不是所有的兼职汪都需要风吹日晒',
+  },
+];
+
+let index = data.length - 1;
+
+const NUM_SECTIONS = 5;
+const NUM_ROWS_PER_SECTION = 5;
+let pageIndex = 0;
 
 class TrainSearch extends React.PureComponent {
 
   constructor(props) {
     super(props);
+
+    const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+    const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+
+    const dataSource = new ListView.DataSource({
+      getRowData,
+      getSectionHeaderData: getSectionData,
+      rowHasChanged: (row1, row2) => row1 !== row2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
+
+    this.dataBlob = {};
+    this.sectionIDs = [];
+    this.rowIDs = [];
+    this.genData = (pIndex = 0) => {
+      for (let i = 0; i < NUM_SECTIONS; i++) {
+        const ii = (pIndex * NUM_SECTIONS) + i;
+        const sectionName = `Section ${ii}`;
+        this.sectionIDs.push(sectionName);
+        this.dataBlob[sectionName] = sectionName;
+        this.rowIDs[ii] = [];
+
+        for (let jj = 0; jj < NUM_ROWS_PER_SECTION; jj++) {
+          const rowName = `S${ii}, R${jj}`;
+          this.rowIDs[ii].push(rowName);
+          this.dataBlob[rowName] = rowName;
+        }
+      }
+      // new object ref
+      this.sectionIDs = [].concat(this.sectionIDs);
+      this.rowIDs = [].concat(this.rowIDs);
+    };
+
     this.state = {
       searchNavibarTitle: this.props.lang.searchNavibarTitle,
       NavibarLeftBack: this.props.lang.NavibarLeftBack,
@@ -17,6 +75,8 @@ class TrainSearch extends React.PureComponent {
       startDate: this.props.startDate,
       fetchTrainsUrl: this.props.lang.fetchTrainsUrl,
       trainsResult: this.props.trainsResult,
+      dataSource: dataSource.cloneWithRowsAndSections(this.dataBlob, this.sectionIDs, this.rowIDs),
+      isLoading: true,
     };
     console.log('TrainSearch 👇');
     console.log(props);
@@ -30,6 +90,14 @@ class TrainSearch extends React.PureComponent {
     Toast.info(this.state.loadingText, 0);
     //抓取车站文本
     this.props.fetchTrains(this.state.fetchTrainsUrl);
+    //列表init：simulate initial Ajax
+    setTimeout(() => {
+      this.genData();
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(this.dataBlob, this.sectionIDs, this.rowIDs),
+        isLoading: false,
+      });
+    }, 600);
   }
 
   componentWillReceiveProps = (nextProps) => {
@@ -46,16 +114,92 @@ class TrainSearch extends React.PureComponent {
     return this.state.fromStation.code != nextState.fromStation.code || 
            this.state.toStation.code != nextState.toStation.code ||
            this.state.startDate != nextState.startDate ||
-           this.state.searchNavibarTitle != nextState.searchNavibarTitle;
+           this.state.searchNavibarTitle != nextState.searchNavibarTitle ||
+           this.state.dataSource != nextState.dataSource;
+  }
+
+  onEndReached = (event) => {
+    // load new data
+    // hasMore: from backend data, indicates whether it is the last page, here is false
+    if (this.state.isLoading && !this.state.hasMore) {
+      return;
+    }
+    console.log('reach end', event);
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      this.genData(++pageIndex);
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRowsAndSections(this.dataBlob, this.sectionIDs, this.rowIDs),
+        isLoading: false,
+      });
+    }, 1000);
   }
 
   render() {
     console.log("render()");
+
+    const separator = (sectionID, rowID) => (
+      <div key={`${sectionID}-${rowID}`}
+        style={{
+          backgroundColor: '#F5F5F9',
+          height: 8,
+          borderTop: '1px solid #ECECED',
+          borderBottom: '1px solid #ECECED',
+        }}
+      />
+    );
+
+    const row = (rowData, sectionID, rowID) => {
+      if (index < 0) {
+        index = data.length - 1;
+      }
+      const obj = data[index--];
+      return (
+        <div key={rowID} className="row">
+          <div className="row-title">{obj.title}</div>
+          <div style={{ display: '-webkit-box', display: 'flex', padding: '0.3rem 0' }}>
+            <img style={{ height: '1.28rem', marginRight: '0.3rem' }} src={obj.img} alt="icon" />
+            <div className="row-text">
+              <div style={{ marginBottom: '0.16rem', fontWeight: 'bold' }}>{obj.des}</div>
+              <div><span style={{ fontSize: '0.6rem', color: '#FF6E27' }}>35</span>¥</div>
+            </div>
+          </div>
+        </div>
+      );
+    };
+
     return (
       <div>
         <NavBar iconName={null} leftContent={this.state.NavibarLeftBack} mode="light" onLeftClick={() => this.props.history.goBack()}>
           <h1 id="TrainIndex-h1">{this.state.searchNavibarTitle}</h1>
         </NavBar>
+        <ListView ref="lv"
+          dataSource={this.state.dataSource}
+          renderHeader={() => <span>header</span>}
+          renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+            {this.state.isLoading ? 'Loading...' : 'Loaded'}
+          </div>)}
+          renderSectionHeader={sectionData => (
+            <div>{`Task ${sectionData.split(' ')[1]}`}</div>
+          )}
+          renderRow={row}
+          renderSeparator={separator}
+          className="icht-am-list"
+          pageSize={4}
+          scrollEventThrottle={20}
+          onScroll={() => { console.log('scroll'); }}
+          onEndReached={this.onEndReached}
+          onEndReachedThreshold={10}
+          stickyHeader
+          stickyProps={{
+            stickyStyle: { zIndex: 999, WebkitTransform: 'none', transform: 'none' },
+            // topOffset: -43,
+            // isActive: false,
+          }}
+          stickyContainerProps={{
+            className: 'for-stickyContainer-demo',
+          }}
+        />
       </div>
     );
   }
