8d2b742d1b0d2a35b870a7d376d653bdcd8d8c20
LMR <59361885@qq.com>
2017-05-20 18:14:39 +0800

    添加注释

--- src/reducers/Trains.js
+++ src/reducers/Trains.js
@@ -4,26 +4,32 @@ import moment from 'moment';
 const trainReducer = (state=initStates, action) => {
   switch(action.type) {
 
+    //设置车站文本。
     case 'SET_STATIONS_TXT':
       return state.set('stationsTxt', action.stationsTxt);
       break;
 
+    //设置出发站。
     case 'SET_FROM_STATION':
       return state.set('fromStation', action.station);
       break;
 
+    //设置目的站。
     case 'SET_TO_STATION':
       return state.set('toStation', action.station);
       break;
 
+    //设置出发时间。
     case 'SET_START_DATE':
       return state.set('startDate', action.moment);
       break;
 
+    //设置抓取的火车车次+价格数据。
     case 'SET_TRAINS_RESULT':
       return state.set('trainsResult', action.json);
       break;
 
+    //按运行时间排序。
     case 'SORT_BY_RUNTIME':
       let rs1 = state.get('trainsResult');
       rs1.result.sort(
@@ -34,6 +40,7 @@ const trainReducer = (state=initStates, action) => {
       return state.set('trainsResult', rs1);
       break;
 
+    //按发车时间排序。
     case 'SORT_BY_STARTTIME':
       let rs2 = state.get('trainsResult');
       rs2.result.sort(
@@ -44,6 +51,7 @@ const trainReducer = (state=initStates, action) => {
       return state.set('trainsResult', rs2);
       break;
 
+    //按最低可预定价格排序。
     case 'SORT_BY_PRICE':
       let rs3 = state.get('trainsResult');
       rs3.result.sort(
